e0a66be58fa72d499b2b48b327e5b87f2ecfe982
Tommy <86784639+tommy88520@users.noreply.github.com>
2023-08-14 12:02:58 +0800

    revise

--- dto/update-postgreql.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostgreqlDto } from './create-postgreql.dto';
-
-export class UpdatePostgreqlDto extends PartialType(CreatePostgreqlDto) {}
--- postgreql.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostgreqlService } from './postgreql.service';
-import { PostgreqlController } from './postgreql.controller';
-
-@Module({
-  controllers: [PostgreqlController],
-  providers: [PostgreqlService]
-})
-export class PostgreqlModule {}
